7da20c8c23f5f40d99f51db4d2a7c7ee84809197
Jayden Wan Kar Ming-2025 <111164351+Starchaser418171514@users.noreply.github.com>
2026-09-11 23:41:54 +0800

    Refactor error message to include help text

--- nugsdiscordbot/src/main/java/com/example/funcs/Add.java
+++ nugsdiscordbot/src/main/java/com/example/funcs/Add.java
@@ -14,6 +14,7 @@ import net.dv8tion.jda.api.entities.channel.middleman.MessageChannel;
 public class Add implements COMMAND {
     JDA api;
     String name = "add";
+    String help = "Use with a name and something to add! Usage: \n`!add \"Character name\" \"skill name\" \"skill name\" ...`";
     String[] parameters;
     MessageChannel channel;
     Message event;
@@ -53,9 +54,7 @@ public class Add implements COMMAND {
         }
 
         if (parameters.length < 3) {
-            channel.sendMessage(
-                    name + " called without enough parameters. Use with a name and something to add! Usage: \n`!add \"Character name\" \"skill name\" \"skill name\" ...`")
-                    .queue();
+            channel.sendMessage(name + " called without enough parameters. " + help).queue();
             return;
         } else {
 
